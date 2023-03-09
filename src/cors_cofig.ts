9da5658d8f8d848e8a9e0e62d7d331e726c2f335
unknown <bruno.emedeiros99@gmail.com>
2023-03-09 15:07:58 -0300

    1.1

--- src/cors_cofig.ts
+++ src/cors_cofig.ts
@@ -1,6 +1,6 @@
 
 import cors from 'cors';
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = ['http://localhost:3000','http://10.90.14.40:3000'];
 
 export const options: cors.CorsOptions = {
   origin: allowedOrigins
